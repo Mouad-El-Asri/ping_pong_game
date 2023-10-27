89c3fb2e02139cf7ae88f6f805c6a2ceb215e5b1
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2023-10-27 07:03:27 +0100

    Modifications :)

--- srcs/boot_game.ts
+++ srcs/boot_game.ts
@@ -16,219 +16,206 @@ const buttons = document.querySelectorAll<HTMLButtonElement>(".btn");
 const exitButton: HTMLButtonElement = document.getElementById("exit-btn") as HTMLButtonElement;
 
 let comp: Player = player_2;
+let gameOver: Boolean = false;
+let userWon: Boolean = false;
+let compWon: Boolean = false;
+let framePerSec: number = 50;
+let isPaused: Boolean = false;
+let renderingStopped: Boolean = false;
+let countdown: number = 3;
 
-class BootPongGame {
-    gameOver: boolean;
-    userWon: boolean;
-    compWon: boolean;
-    framePerSec: number;
-    isPaused: boolean;
-	renderingStopped: boolean;
-	countdown: number;
+function pauseGame(duration: number): void {
+    isPaused = true;
+    setTimeout(() => {
+        isPaused = false;
+    }, duration);
+}
 
-    constructor() {
-        this.gameOver = false;
-        this.userWon = false;
-        this.compWon = false;
-        this.framePerSec = 50;
-        this.isPaused = false;
-		this.renderingStopped = false;
-		this.countdown = 3;
-    }
+function collision(ball: Ball, player: Player): boolean {
+    const playerTop: number = player.y;
+    const playerBottom: number = player.y + player.h;
+    const playerLeft: number = player.x;
+    const playerRight: number = player.x + player.w;
 
-    pauseGame(duration: number): void {
-        this.isPaused = true;
-        setTimeout(() => {
-            this.isPaused = false;
-        }, duration);
-    }
+    const ballTop: number = ball.y - ball.r;
+    const ballBottom: number = ball.y + ball.r;
+    const ballLeft: number = ball.x - ball.r;
+    const ballRight: number = ball.x + ball.r;
 
-    collision(ball: Ball, player: Player): boolean {
-        const playerTop: number = player.y;
-        const playerBottom: number = player.y + player.h;
-        const playerLeft: number = player.x;
-        const playerRight: number = player.x + player.w;
+    return (
+        ballRight > playerLeft &&
+        ballTop < playerBottom &&
+        ballLeft < playerRight &&
+        ballBottom > playerTop
+    );
+}
 
-        const ballTop: number = ball.y - ball.r;
-        const ballBottom: number = ball.y + ball.r;
-        const ballLeft: number = ball.x - ball.r;
-        const ballRight: number = ball.x + ball.r;
+function resetBall(): void {
+    ball.x = canvasWidth / 2;
+    ball.y = canvasHeight / 2;
+    ball.velocityX *= -1;
+}
 
-        return (
-            ballRight > playerLeft &&
-            ballTop < playerBottom &&
-            ballLeft < playerRight &&
-            ballBottom > playerTop
-        );
-    }
+function movePaddle(event: MouseEvent): void {
+    let pos: DOMRect = canvas.getBoundingClientRect();
+    player_1.y = event.clientY - pos.top - player_1.h / 2;
+}
 
-    resetBall(): void {
-        ball.x = canvasWidth / 2;
-        ball.y = canvasHeight / 2;
-        ball.velocityX *= -1;
-    }
-
-    movePaddle(event: MouseEvent): void {
-        let pos: DOMRect = canvas.getBoundingClientRect();
-        player_1.y = event.clientY - pos.top - player_1.h / 2;
-    }
-
-	movePaddleWithKeys(event: KeyboardEvent): void {
-		if (event.key == "ArrowDown") {
-			player_1.y += 30;
-			if (player_1.y >= canvasHeight - player_1.h) {
-				player_1.y = canvasHeight - player_1.h / 2;
-			}
-		} else if (event.key == "ArrowUp") {
-			player_1.y -= 30;
-			if (player_1.y <= -player_1.h / 2) {
-				player_1.y = -player_1.h / 2;
-			}
+function movePaddleWithKeys(event: KeyboardEvent): void {
+	if (event.key == "ArrowDown") {
+		player_1.y += 30;
+		if (player_1.y >= canvasHeight - player_1.h) {
+			player_1.y = canvasHeight - player_1.h / 2;
 		}
-    }
-
-    updateScore(): void {
-        if (ball.x - ball.r < 0) {
-            comp.score++;
-            this.resetBall();
-            this.pauseGame(150);
-        } else if (ball.x + ball.r > canvasWidth) {
-            player_1.score++;
-            this.resetBall();
-            this.pauseGame(150);
-        }
-    }
-
-    checkGameStatus(): void {
-        if (player_1.score === 5) {
-            this.userWon = true;
-            this.gameOver = true;
-        } else if (comp.score === 5) {
-            this.compWon = true;
-            this.gameOver = true;
-        }
-    }
-
-    update(): void {
-        ball.x += ball.velocityX;
-        ball.y += ball.velocityY;
-
-        let computerLevel: number = 0.1;
-
-        comp.y += (ball.y - (comp.y + comp.h / 2)) * computerLevel;
-
-        if (ball.y + ball.r > canvasHeight || ball.y + ball.r < 10) {
-            ball.velocityY *= -1;
-        }
-
-        let player: Player = ball.x < canvasWidth / 2 ? player_1 : comp;
-
-        if (this.collision(ball, player)) {
-            // where the ball hits the player
-            let collidePoint: number = ball.y - (player.y + player.h / 2);
-
-            // normalization
-            collidePoint = collidePoint / (player.h / 2);
-
-            // calculate the angle in Radian
-			let angleRad = (Math.PI / 4) * collidePoint;
-			if (player === player_1) {
-				angleRad *= 1;
-			} else if (player === comp) {
-				angleRad *= -1;
-			}
-
-            let direction: number = ball.x < canvasWidth / 2 ? 1 : -1;
-
-            ball.velocityX = direction * ball.speed * Math.cos(angleRad);
-            ball.velocityY = direction * ball.speed * Math.sin(angleRad);
-
-            ball.speed += 0.2;
-        }
-
-        this.updateScore();
-        this.checkGameStatus();
-    }
-
-    render(): void {
-        if (this.gameOver) {
-            drawRect(0, 0, canvasWidth, canvasHeight, "#B2C6E4");
-            drawLine(
-                canvasWidth / 2,
-                0,
-                canvasWidth / 2,
-                canvasHeight / 2 - 70,
-                midLine.color
-            );
-            drawLine(
-                canvasWidth / 2,
-                canvasHeight / 2 + 70,
-                canvasWidth / 2,
-                canvasHeight,
-                midLine.color
-            );
-
-			this.renderingStopped = true;
-            if (this.userWon) {
-				message.innerHTML = "Game Over, You Won!";
-            } else if (this.compWon) {
-                message.innerHTML = "Game Over, You Lost!";
-            }
-			exitButton.style.display = "block";
-        } else {
-            drawRect(0, 0, canvasWidth, canvasHeight, "#B2C6E4");
-            drawRect(player_1.x, player_1.y, player_1.w, player_1.h, player_1.color);
-            drawRect(comp.x, comp.y, comp.w, comp.h, comp.color);
-            drawLine(
-                midLine.startX,
-                midLine.startY,
-                midLine.endX,
-                midLine.endY,
-                midLine.color
-            );
-            drawBall(ball.x, ball.y, ball.r, ball.color);
-            drawScore(player_1.score.toString(), -50, 70, "#201E3A");
-            drawScore(comp.score.toString(), 50, 70, "#201E3A");
-        }
-    }
-
-    game(): void {
-        if (!this.isPaused) {
-            this.update();
-        }
-        this.render();
-    }
-
-    startGame(): void {
-		for (const button of buttons) {
-			button.style.display = "none";
+	} else if (event.key == "ArrowUp") {
+		player_1.y -= 30;
+		if (player_1.y <= -player_1.h / 2) {
+			player_1.y = -player_1.h / 2;
 		}
-		console.log("Starting Game");
-		message.innerHTML = `The game will start in ${this.countdown} seconds...`;
-		let countdownInterval = setInterval(() => {
-			this.countdown--;
-			if (this.countdown) {
-				message.innerHTML = `The game will start in ${this.countdown} seconds...`;
-			} else {
-				clearInterval(countdownInterval);
-				message.innerHTML = "";
-			}
-		}, 1000);
-		
-		setTimeout(() => {
-			canvas.addEventListener("mousemove", this.movePaddle);
-			window.addEventListener("keydown", this.movePaddleWithKeys);
-	
-			this.pauseGame(500);
-			let interval = setInterval(() => {
-				if (this.renderingStopped) {
-					console.log("hello");
-					clearInterval(interval);
-				}
-				this.game();
-			}, 1000 / this.framePerSec);
-		}, 3100);
+	}
+}
+
+function updateScore(): void {
+    if (ball.x - ball.r < 0) {
+        comp.score++;
+        resetBall();
+        pauseGame(150);
+    } else if (ball.x + ball.r > canvasWidth) {
+        player_1.score++;
+        resetBall();
+        pauseGame(150);
     }
 }
 
-export default BootPongGame;
+function checkGameStatus(): void {
+    if (player_1.score === 5) {
+        userWon = true;
+        gameOver = true;
+    } else if (comp.score === 5) {
+        compWon = true;
+        gameOver = true;
+    }
+}
+
+function update(): void {
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+
+    let computerLevel: number = 0.1;
+
+    comp.y += (ball.y - (comp.y + comp.h / 2)) * computerLevel;
+
+    if (ball.y + ball.r > canvasHeight || ball.y + ball.r < 10) {
+        ball.velocityY *= -1;
+    }
+
+    let player: Player = ball.x < canvasWidth / 2 ? player_1 : comp;
+
+    if (collision(ball, player)) {
+        // where the ball hits the player
+        let collidePoint: number = ball.y - (player.y + player.h / 2);
+
+        // normalization
+        collidePoint = collidePoint / (player.h / 2);
+
+        // calculate the angle in Radian
+		let angleRad = (Math.PI / 4) * collidePoint;
+		if (player === player_1) {
+			angleRad *= 1;
+		} else if (player === comp) {
+			angleRad *= -1;
+		}
+
+        let direction: number = ball.x < canvasWidth / 2 ? 1 : -1;
+
+        ball.velocityX = direction * ball.speed * Math.cos(angleRad);
+        ball.velocityY = direction * ball.speed * Math.sin(angleRad);
+
+        ball.speed += 0.2;
+    }
+
+    updateScore();
+    checkGameStatus();
+}
+
+function render(): void {
+    if (gameOver) {
+        drawRect(0, 0, canvasWidth, canvasHeight, "#B2C6E4");
+        drawLine(
+            canvasWidth / 2,
+            0,
+            canvasWidth / 2,
+            canvasHeight / 2 - 70,
+            midLine.color
+        );
+        drawLine(
+            canvasWidth / 2,
+            canvasHeight / 2 + 70,
+            canvasWidth / 2,
+            canvasHeight,
+            midLine.color
+        );
+
+		renderingStopped = true;
+        if (userWon) {
+			message.innerHTML = "Game Over, You Won!";
+        } else if (compWon) {
+            message.innerHTML = "Game Over, You Lost!";
+        }
+		exitButton.style.display = "block";
+    } else {
+        drawRect(0, 0, canvasWidth, canvasHeight, "#B2C6E4");
+        drawRect(player_1.x, player_1.y, player_1.w, player_1.h, player_1.color);
+        drawRect(comp.x, comp.y, comp.w, comp.h, comp.color);
+        drawLine(
+            midLine.startX,
+            midLine.startY,
+            midLine.endX,
+            midLine.endY,
+            midLine.color
+        );
+        drawBall(ball.x, ball.y, ball.r, ball.color);
+        drawScore(player_1.score.toString(), -50, 70, "#201E3A");
+        drawScore(comp.score.toString(), 50, 70, "#201E3A");
+    }
+}
+
+function game(): void {
+    if (!isPaused) {
+        update();
+    }
+    render();
+}
+
+function startBotGame(): void {
+	for (const button of buttons) {
+		button.style.display = "none";
+	}
+	console.log("Starting Game");
+	message.innerHTML = `The game will start in ${countdown} seconds...`;
+	let countdownInterval = setInterval(() => {
+		countdown--;
+		if (countdown) {
+			message.innerHTML = `The game will start in ${countdown} seconds...`;
+		} else {
+			clearInterval(countdownInterval);
+			message.innerHTML = "";
+		}
+	}, 1000);
+
+	setTimeout(() => {
+		canvas.addEventListener("mousemove", movePaddle);
+		window.addEventListener("keydown", movePaddleWithKeys);
+
+		pauseGame(500);
+		let interval = setInterval(() => {
+			if (renderingStopped) {
+				console.log("hello");
+				clearInterval(interval);
+			}
+			game();
+		}, 1000 / framePerSec);
+	}, 3100);
+}
+
+export default startBotGame;
--- srcs/index.tsx
+++ srcs/index.tsx
@@ -5,13 +5,10 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 import startGame from './multiplayer_game';
-import BootPongGame from "./boot_game";
+import startBotGame from "./boot_game";
 
 const mainButton: HTMLElement = document.getElementById("online-game") as HTMLElement;
 const secondaryButton: HTMLElement = document.getElementById("bot-game") as HTMLElement;
 
 mainButton.addEventListener("click", startGame);
-secondaryButton.addEventListener("click", () => {
-	const game: BootPongGame = new BootPongGame();
-	game.startGame();
-});
+secondaryButton.addEventListener("click", startBotGame);
